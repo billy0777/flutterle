757dcefc2a1bef9e0c16785c95ce866f0f53ccbf
Pankaj Acharya <acharyapankaj897@gmail.com>
2024-01-30 14:15:02 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dorya/pages/homepage.dart';
 import 'package:dorya/pages/settingpage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
       routes: {
-        "/": (context) => HomePage(),
-        "/setting": (context) => SettingPage()
+        "/homepage": (context) => HomePage(),
+        "/": (context) => SettingPage()
       },
     );
   }
--- lib/pages/settingpage.dart
+++ lib/pages/settingpage.dart
@@ -6,7 +6,44 @@ class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("Setting")),
+      child: Center(
+          child: Column(
+        children: [
+          SizedBox(height: 20),
+          Image.asset("assetz/images/loginimg.png", fit: BoxFit.cover),
+          SizedBox(height: 20),
+          Text(
+            "Welcome to Thanwala",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 18),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter Your User Name", labelText: "User"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Your Password", labelText: "Password"),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style:
+                      TextButton.styleFrom(backgroundColor: Colors.deepPurple),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
